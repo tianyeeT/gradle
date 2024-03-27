54bdff4a18a178aade7b0ea2cbf981db19b1612c
Vlad Chesnokov <vchesnokov@gradle.com>
2024-03-27 19:36:55 +0400

    WIP: allow project-cache-dir if the dir is on the same FS

--- platforms/core-execution/file-watching/src/main/java/org/gradle/internal/watch/registry/impl/DefaultFileWatcherProbeRegistry.java
+++ platforms/core-execution/file-watching/src/main/java/org/gradle/internal/watch/registry/impl/DefaultFileWatcherProbeRegistry.java
@@ -26,29 +26,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class DefaultFileWatcherProbeRegistry implements FileWatcherProbeRegistry {
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultFileWatcherProbeRegistry.class);
 
-    private final Map<File, WatchProbe> watchProbesByHierarchy = new ConcurrentHashMap<>();
+    private final Map<File, WatchProbe> watchProbesByHierarchy = new ConcurrentHashMap<>(); //TODO: refactor: split by probe file
     private final Map<String, WatchProbe> watchProbesByPath = new ConcurrentHashMap<>();
 
-    private final Function<File, File> probeLocationResolver;
-
-    public DefaultFileWatcherProbeRegistry(Function<File, File> probeLocationResolver) {
-        this.probeLocationResolver = probeLocationResolver;
-    }
-
     @Override
-    public void registerProbe(File watchableHierarchy) {
+    public void registerProbe(File watchableHierarchy, File probeLocation) {
         if (watchProbesByHierarchy.containsKey(watchableHierarchy)) {
             // Already registered
             return;
         }
         LOGGER.debug("Registering probe for {}", watchableHierarchy);
-        File probeFile = probeLocationResolver.apply(watchableHierarchy);
+        File probeFile = new File(probeLocation, "file-system.probe"); //TODO: refactor: multiple hierarchies can have the same probe
         WatchProbe watchProbe = new WatchProbe(watchableHierarchy, probeFile);
         watchProbesByHierarchy.put(watchableHierarchy, watchProbe);
         watchProbesByPath.put(probeFile.getAbsolutePath(), watchProbe);
